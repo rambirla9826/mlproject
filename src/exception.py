b9e0193968558155d6d1c78922088b1bfebaa5a9
rambirla9826 <rambirla2802@gmail.com>
2026-06-24 01:12:51 +0530

    Completed EDA environment setup

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,9 @@
 import sys 
-import logging
+from src.logger import logging 
 
 def error_message_detail(error,error_detail:sys):
-   _,_,exe_tb=error_detail.exe_info()
-   file_name=exe_tb.tb_frame.f_code.co_filenmae
+   _,_,exe_tb=error_detail.exc_info()
+   file_name=exe_tb.tb_frame.f_code.co_filename
    error_message="error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,exe_tb.tb_lineno,str(error))
    
@@ -18,5 +18,10 @@ class CustomeException(Exception):
     def __str__(self):
         return self.error_message
     
-
+# if __name__=="__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by zero")
+#         raise CustomeException(e,sys)
     
